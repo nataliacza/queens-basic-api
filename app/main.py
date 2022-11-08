f9d5276ec60f77f961507d4363e66ecc21f93795
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-08 13:03:46 +0100

    endpoints: get all categories, cities and get single result

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.db_memory import category_db, city_db, queen_db
 from app.schemas import City, CitySave, Category, CategorySave, Queen, QueenSave
 
 
-app = FastAPI(title="Drag Queens Base API")
+app = FastAPI(title="Drag Queens Basic API")
 
 
 # region Queens
@@ -34,6 +34,54 @@ async def get_queen(queen_id: UUID):
         return result
     return JSONResponse(status_code=404, content={"code": "404",
                                                   "message": "Not Found"})
+
+# endregion
+
+# region Cities
+
+@app.get("/v1/cities/",
+         tags=["Cities"],
+         status_code=200,
+         response_model=List[City])
+async def get_all_cities():
+    result = [city for city in city_db.values()]
+    return result
+
+
+@app.get("/v1/cities/{city_id}",
+         tags=["Cities"],
+         status_code=200,
+         responses={"404": {"code": "404", "message": "Not Found"}},
+         response_model=City)
+async def get_city(city_id: UUID):
+    result = city_db.get(str(city_id))
+    if result:
+        return result
+    return JSONResponse(status_code=404, content={"code": "404", "message": "Not Found"})
+
+# endregion
+
+# region Categories
+
+@app.get("/v1/categories/",
+         tags=["Categories"],
+         status_code=200,
+         response_model=List[Category])
+async def get_all_categories():
+    result = [category for category in category_db.values()]
+    return result
+
+
+@app.get("/v1/categories/{category_id}",
+         tags=["Categories"],
+         status_code=200,
+         responses={"404": {"code": "404", "message": "Not Found"}},
+         response_model=Category)
+async def get_category(category_id: UUID):
+    result = category_db.get(str(category_id))
+    if result:
+        return result
+    return JSONResponse(status_code=404, content={"code": "404", "message": "Not Found"})
 
 # endregion
 
