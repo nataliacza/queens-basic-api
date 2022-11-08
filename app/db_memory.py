a298e19c1f529e3325d9ff40b7e4c8a808f564d5
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-08 12:15:16 +0100

    wip: add schemas and dummy-data

---
+++ app/db_memory.py
@@ -0,0 +1,61 @@
+
+queen_db = {
+    "30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1": {
+        "queen_id": "30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1",
+        "nickname": "Twoja Stara",
+        "status": "Active",
+        "info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut...",
+        "on_stage_since": 2000,
+        "hometown": {
+            "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+            "name": "Warszawa",
+            "region": "Mazowieckie",
+            "country": "Polska"
+        },
+        "residence": {
+            "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+            "name": "Warszawa",
+            "region": "Mazowieckie",
+            "country": "Polska"
+        },
+        "email": "twoja@stara.pl",
+        "web": "https://www.twoja-stara.com",
+        "instagram": "https://www.instagram.com/twoja___stara/",
+        "facebook": "https://www.facebook.com/TwojaStaraDQ/",
+        "twitter": "https://www.twitter.com/twoja-stara/",
+        "tags": [
+            {"category_id": "8898ea70-2c02-4551-9658-691e3293c516",
+             "name": "glam"}
+        ]
+    }
+}
+
+category_db = {
+    "8898ea70-2c02-4551-9658-691e3293c516": {
+        "category_id": "8898ea70-2c02-4551-9658-691e3293c516",
+        "name": "glam"
+    },
+    "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2": {
+        "category_id": "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2",
+        "name": "sing"
+    },
+    "57940578-16f4-45ec-9ac4-ad2e45b11b35": {
+        "category_id": "57940578-16f4-45ec-9ac4-ad2e45b11b35",
+        "name": "camp"
+    },
+}
+
+city_db = {
+    "eb195501-603e-46f4-b3f1-71f7de304a28": {
+        "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+        "name": "Warszawa",
+        "region": "Mazowieckie",
+        "country": "Polska"
+    },
+    "7ced7771-64ff-44e5-8a87-c0f4126a6328": {
+        "city_id": "7ced7771-64ff-44e5-8a87-c0f4126a6328",
+        "name": "Kraków",
+        "region": "Małopolskie",
+        "country": "Polska"
+    }
+}
