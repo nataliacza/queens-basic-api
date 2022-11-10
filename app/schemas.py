10ec268c9eaa04853b184890b6a14fde754793f6
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-10 10:32:58 +0100

    refactor

--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,13 @@
+import datetime
 from enum import Enum
 from typing import List
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, HttpUrl, Field
+from pydantic import BaseModel, EmailStr, HttpUrl, Field, UUID4
 
+
+def get_current_year():
+    return datetime.datetime.utcnow().year
 
 class StatusEnum(str, Enum):
     Active = "Active"
@@ -11,62 +15,55 @@ class StatusEnum(str, Enum):
     Unknown = "Unknown"
 
 
-class City(BaseModel):
-    city_id: UUID
-    name: str
-    region: str
-    country: str
-
-
 class CitySave(BaseModel):
-    name: str
-    region: str
-    country: str
+    name: str = Field(max_length=50)
+    region: str = Field(max_length=50)
+    country: str = Field(max_length=50)
 
 
-class Category(BaseModel):
-    category_id: UUID
-    name: str
+class City(CitySave):
+    city_id: UUID4
 
 
 class CategorySave(BaseModel):
-    name: str
+    name: str = Field(min_length=3, max_length=10)
 
 
-class Queen(BaseModel):
-    queen_id: UUID
-    nickname: str
+class Category(CategorySave):
+    category_id: UUID4
+
+
+class QueenId(BaseModel):
+    queen_id: UUID4
+
+
+class QueenBase(QueenId):
+    nickname: str = Field(min_length=2, max_length=40)
     status: StatusEnum = Field(default=StatusEnum.Unknown)
-    info: str = Field(default=None)
-    on_stage_since: int = Field(default=None)
+    on_stage_since: int = Field(default=None, le=get_current_year(), gt=get_current_year()-500)
+    tags: List[Category] = Field(default=[], unique_items=True)
+
+
+class QueenSocial(BaseModel):
+    email: EmailStr = Field(default=None)
+    web: HttpUrl = Field(default=None)
+    instagram: HttpUrl = Field(default=None)
+    facebook: HttpUrl = Field(default=None)
+    twitter: HttpUrl = Field(default=None)
+
+
+class Queen(QueenBase, QueenSocial, QueenId):
+    nickname: str = Field(min_length=2, max_length=40)
+    info: str = Field(default=None, max_length=500)
     hometown: City = Field(default=None)
     residence: City = Field(default=None)
-    email: EmailStr = Field(default=None)
-    web: HttpUrl = Field(default=None)
-    instagram: HttpUrl = Field(default=None)
-    facebook: HttpUrl = Field(default=None)
-    twitter: HttpUrl = Field(default=None)
-    tags: List[Category] = Field(default=[])
 
 
-class QueenSave(BaseModel):
-    nickname: str
+class QueenSave(QueenSocial):
+    nickname: str = Field(min_length=2, max_length=40)
     status: StatusEnum = Field(default=StatusEnum.Unknown)
-    info: str = Field(default=None)
-    on_stage_since: int = Field(default=None)
-    hometown: UUID = Field(default=None)
-    residence: UUID = Field(default=None)
-    email: EmailStr = Field(default=None)
-    web: HttpUrl = Field(default=None)
-    instagram: HttpUrl = Field(default=None)
-    facebook: HttpUrl = Field(default=None)
-    twitter: HttpUrl = Field(default=None)
-    tags: List[UUID] = Field(default=[])
-
-
-class QueenBase(BaseModel):
-    queen_id: UUID
-    nickname: str
-    status: StatusEnum = Field(default=StatusEnum.Unknown)
-    on_stage_since: int = Field(default=None)
-    tags: List[Category] = Field(default=[])
+    info: str = Field(default=None, max_length=500)
+    on_stage_since: int = Field(default=None, le=get_current_year(), gt=get_current_year()-500)
+    hometown: UUID4 = Field(default=None)
+    residence: UUID4 = Field(default=None)
+    tags: List[UUID4] = Field(default=[], unique_items=True)
