9b73349287f5f658d6e349d2f8bda05d40832330
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-11 12:33:51 +0100

    refactor ids in db so they are UUID objects

--- app/db_memory.py
+++ app/db_memory.py
@@ -1,18 +1,19 @@
+from uuid import UUID
 
 queen_db = {
-    "30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1": {
-        "queen_id": "30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1",
+    UUID("30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1"): {
+        "queen_id": UUID("30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1"),
         "nickname": "Twoja Stara",
         "status": "Active",
         "info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut...",
         "on_stage_since": 2000,
         "hometown": {
-            "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+            "city_id": UUID("eb195501-603e-46f4-b3f1-71f7de304a28"),
             "name": "Warszawa",
             "region": "Mazowieckie",
             "country": "Polska"},
         "residence": {
-            "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+            "city_id": UUID("eb195501-603e-46f4-b3f1-71f7de304a28"),
             "name": "Warszawa",
             "region": "Mazowieckie",
             "country": "Polska"},
@@ -22,38 +23,38 @@ queen_db = {
         "facebook": "https://www.facebook.com/TwojaStaraDQ/",
         "twitter": "https://www.twitter.com/twoja-stara/",
         "tags": [
-            {"category_id": "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2",
+            {"category_id": UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"),
              "name": "sing"},
-            {"category_id": "8898ea70-2c02-4551-9658-691e3293c516",
+            {"category_id": UUID("8898ea70-2c02-4551-9658-691e3293c516"),
              "name": "glam"}
         ]
     }
 }
 
 category_db = {
-    "8898ea70-2c02-4551-9658-691e3293c516": {
-        "category_id": "8898ea70-2c02-4551-9658-691e3293c516",
+    UUID("8898ea70-2c02-4551-9658-691e3293c516"): {
+        "category_id": UUID("8898ea70-2c02-4551-9658-691e3293c516"),
         "name": "glam"
     },
-    "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2": {
+    UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"): {
         "category_id": "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2",
         "name": "sing"
     },
-    "57940578-16f4-45ec-9ac4-ad2e45b11b35": {
+    UUID("57940578-16f4-45ec-9ac4-ad2e45b11b35"): {
         "category_id": "57940578-16f4-45ec-9ac4-ad2e45b11b35",
         "name": "camp"
     },
 }
 
 city_db = {
-    "eb195501-603e-46f4-b3f1-71f7de304a28": {
-        "city_id": "eb195501-603e-46f4-b3f1-71f7de304a28",
+    UUID("eb195501-603e-46f4-b3f1-71f7de304a28"): {
+        "city_id": UUID("eb195501-603e-46f4-b3f1-71f7de304a28"),
         "name": "Warszawa",
         "region": "Mazowieckie",
         "country": "Polska"
     },
-    "7ced7771-64ff-44e5-8a87-c0f4126a6328": {
-        "city_id": "7ced7771-64ff-44e5-8a87-c0f4126a6328",
+    UUID("7ced7771-64ff-44e5-8a87-c0f4126a6328"): {
+        "city_id": UUID("7ced7771-64ff-44e5-8a87-c0f4126a6328"),
         "name": "Kraków",
         "region": "Małopolskie",
         "country": "Polska"
