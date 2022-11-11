4bb8c2ba7ebec4b05c37aac79570731f83403d60
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-11 13:35:11 +0100

    add records to db

--- app/db_memory.py
+++ app/db_memory.py
@@ -1,6 +1,46 @@
 from uuid import UUID
 
 queen_db = {
+    UUID("d43ee99a-038e-4381-8b10-b014ff7253cd"): {
+        "queen_id": UUID("d43ee99a-038e-4381-8b10-b014ff7253cd"),
+        "nickname": "Hrabina",
+        "status": "Active",
+        "info": None,
+        "on_stage_since": None,
+        "hometown": None,
+        "residence": None,
+        "email": "TheCountessDQ@gmail.com",
+        "web": None,
+        "instagram": "https://www.instagram.com/hrabina.drag/",
+        "facebook": "https://www.facebook.com/CountesRose/",
+        "twitter": None,
+        "tags": [
+            {"category_id": UUID("f82466c4-4a4c-4a08-aaec-00667324e3bf"),
+             "name": "wig"},
+            {"category_id": UUID("8898ea70-2c02-4551-9658-691e3293c516"),
+             "name": "glam"}
+        ]
+    },
+    UUID("6c9463b7-bf69-4f8d-87dd-f1aa10a27762"): {
+        "queen_id": UUID("6c9463b7-bf69-4f8d-87dd-f1aa10a27762"),
+        "nickname": "Misia Joachim",
+        "status": "Active",
+        "info": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut...",
+        "on_stage_since": 2011,
+        "hometown": None,
+        "residence": None,
+        "email": "joachimmakeup@gmail.com",
+        "web": None,
+        "instagram": "https://www.instagram.com/misiajoachim/",
+        "facebook": "https://www.facebook.com/profile.php?id=100071518396045",
+        "twitter": None,
+        "tags": [
+            {"category_id": UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"),
+             "name": "makeup"},
+            {"category_id": UUID("8898ea70-2c02-4551-9658-691e3293c516"),
+             "name": "glam"}
+        ]
+    },
     UUID("30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1"): {
         "queen_id": UUID("30c9c75d-dd6c-47fa-b932-e5a5c3fc08c1"),
         "nickname": "Twoja Stara",
@@ -23,10 +63,8 @@ queen_db = {
         "facebook": "https://www.facebook.com/TwojaStaraDQ/",
         "twitter": "https://www.twitter.com/twoja-stara/",
         "tags": [
-            {"category_id": UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"),
-             "name": "sing"},
-            {"category_id": UUID("8898ea70-2c02-4551-9658-691e3293c516"),
-             "name": "glam"}
+            {"category_id": UUID("57940578-16f4-45ec-9ac4-ad2e45b11b35"),
+             "name": "camp"}
         ]
     }
 }
@@ -37,13 +75,17 @@ category_db = {
         "name": "glam"
     },
     UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"): {
-        "category_id": "b11ab87c-b7d5-4dd9-a640-0bbe545abfe2",
-        "name": "sing"
+        "category_id": UUID("b11ab87c-b7d5-4dd9-a640-0bbe545abfe2"),
+        "name": "makeup"
     },
     UUID("57940578-16f4-45ec-9ac4-ad2e45b11b35"): {
-        "category_id": "57940578-16f4-45ec-9ac4-ad2e45b11b35",
+        "category_id": UUID("57940578-16f4-45ec-9ac4-ad2e45b11b35"),
         "name": "camp"
     },
+    UUID("f82466c4-4a4c-4a08-aaec-00667324e3bf"): {
+        "category_id": UUID("f82466c4-4a4c-4a08-aaec-00667324e3bf"),
+        "name": "wig"
+    }
 }
 
 city_db = {
