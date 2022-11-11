959ea29e81e5e2700ef475a9a5cc9011f3864f08
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-11 12:39:10 +0100

    fix: fixes in main as per UUID changes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from typing import List
 from uuid import UUID
 
-import uvicorn
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
 from app.db_memory import queen_db, city_db, category_db
-from app.helpers import (generate_new_uuid4, is_unique, is_assigned, update_city_in_queens_db,
-                         delete_tag_from_queens_db, update_tag_name_in_queens_db, is_able_to_add_to_db)
+from app.helpers import (is_unique, is_assigned, update_city_in_queens_db, delete_tag_from_queens_db,
+                         update_tag_name_in_queens_db, able_to_add_to_db)
 from app.schemas import Queen, QueenSave, City, CitySave, Category, CategorySave, QueenBase
 
 app = FastAPI(title="Drag Queens Basic API")
@@ -30,7 +29,7 @@ async def get_all_queens():
          responses={"404": {"code": "404", "message": "Not Found"}},
          response_model=Queen)
 async def get_queen(queen_id: UUID):
-    result = queen_db.get(str(queen_id))
+    result = queen_db.get(queen_id)
     if result:
         return result
     return JSONResponse(status_code=404, content={"code": "404",
@@ -51,7 +50,7 @@ async def add_queen(queen_details: QueenSave):
 
     while error["status"] is False:
         if queen_details.hometown is not None:
-            get_hometown = city_db.get(str(queen_details.hometown))
+            get_hometown = city_db.get(queen_details.hometown)
             if get_hometown is None:
                 error["status"] = True
                 error["reason"] = "Hometown City"
@@ -60,7 +59,7 @@ async def add_queen(queen_details: QueenSave):
                 hometown = get_hometown
 
         if queen_details.residence is not None:
-            get_residence = city_db.get(str(queen_details.residence))
+            get_residence = city_db.get(queen_details.residence)
             if get_residence is None:
                 error["status"] = True
                 error["reason"] = "Residence City"
@@ -70,7 +69,7 @@ async def add_queen(queen_details: QueenSave):
 
         if len(queen_details.tags) > 0:
             for tag_id in queen_details.tags:
-                get_tag = category_db.get(str(tag_id))
+                get_tag = category_db.get(tag_id)
                 if get_tag is None:
                     error["status"] = True
                     error["reason"] = "Tag"
@@ -83,12 +82,11 @@ async def add_queen(queen_details: QueenSave):
         return JSONResponse(status_code=404, content={"code": "404",
                                                       "message": f"{error['reason']} Id Not Found"})
 
-    if not is_able_to_add_to_db(queen_db):
+    if not able_to_add_to_db(queen_db):
         return JSONResponse(status_code=400,
                             content={"code": "400", "message": "Number of items in db was exceeded"})
 
-    new_object = Queen(queen_id=generate_new_uuid4(),
-                       nickname=queen_details.nickname,
+    new_object = Queen(nickname=queen_details.nickname.title(),
                        status=queen_details.status,
                        info=queen_details.info,
                        on_stage_since=queen_details.on_stage_since,
@@ -101,7 +99,7 @@ async def add_queen(queen_details: QueenSave):
                        twitter=queen_details.twitter,
                        tags=tags)
 
-    queen_db[str(new_object.queen_id)] = new_object.dict()
+    queen_db[new_object.queen_id] = new_object.dict()
 
     return new_object
 
@@ -113,7 +111,7 @@ async def add_queen(queen_details: QueenSave):
          response_model=Queen)
 async def update_queen(queen_id: UUID, queen_details: QueenSave):
 
-    find_queen = queen_db.get(str(queen_id))
+    find_queen = queen_db.get(queen_id)
     hometown = None
     residence = None
     tags = []
@@ -126,7 +124,7 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
             break
 
         if queen_details.hometown is not None:
-            get_hometown = city_db.get(str(queen_details.hometown))
+            get_hometown = city_db.get(queen_details.hometown)
             if get_hometown is None:
                 error["status"] = True
                 error["reason"] = "Hometown City"
@@ -135,7 +133,7 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
                 hometown = get_hometown
 
         if queen_details.residence is not None:
-            get_residence = city_db.get(str(queen_details.residence))
+            get_residence = city_db.get(queen_details.residence)
             if get_residence is None:
                 error["status"] = True
                 error["reason"] = "Residence City"
@@ -145,7 +143,7 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
 
         if len(queen_details.tags) > 0:
             for tag_id in queen_details.tags:
-                get_tag = category_db.get(str(tag_id))
+                get_tag = category_db.get(tag_id)
                 if get_tag is None:
                     error["status"] = True
                     error["reason"] = "Tag"
@@ -159,6 +157,7 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
                                                       "message": f"{error['reason']} Id Not Found"})
 
     find_queen.update(queen_details.dict())
+    find_queen["nickname"] = queen_details.nickname.title()
 
     if hometown is not None:
         find_queen["hometown"] = hometown
@@ -169,7 +168,7 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
     if len(tags) > 0:
         find_queen["tags"] = tags
 
-    queen_db[str(queen_id)] = find_queen
+    queen_db[queen_id] = find_queen
 
     return find_queen
 
@@ -179,9 +178,9 @@ async def update_queen(queen_id: UUID, queen_details: QueenSave):
             status_code=204,
             responses={"404": {"code": "404", "message": "Not Found"}})
 async def delete_queen(queen_id: UUID):
-    find_queen = queen_db.get(str(queen_id))
+    find_queen = queen_db.get(queen_id)
     if find_queen:
-        del queen_db[str(queen_id)]
+        del queen_db[queen_id]
         return {}
 
     return JSONResponse(status_code=404, content={"code": "404",
@@ -206,13 +205,13 @@ async def get_all_cities():
          responses={"404": {"code": "404", "message": "Not Found"}},
          response_model=City)
 async def get_city(city_id: UUID):
-    result = city_db.get(str(city_id))
+    result = city_db.get(city_id)
     if result:
         return result
     return JSONResponse(status_code=404, content={"code": "404", "message": "Not Found"})
 
 
-@app.post("/api/api/v1/cities/",
+@app.post("/api/v1/cities/",
           tags=["Cities"],
           status_code=201,
           responses={"400": {"code": "400", "message": "Bad Request"}},
@@ -227,16 +226,15 @@ async def add_city(city_details: CitySave):
         return JSONResponse(status_code=400, content={"code": "400",
                                                       "message": "Name must be unique"})
 
-    if not is_able_to_add_to_db(city_db):
+    if not able_to_add_to_db(city_db):
         return JSONResponse(status_code=400,
                             content={"code": "400", "message": "Number of items in db was exceeded"})
 
-    new_object = City(city_id=generate_new_uuid4(),
-                      name=city_details.name.title().strip(),
+    new_object = City(name=city_details.name.title().strip(),
                       region=city_details.region.title().strip(),
                       country=city_details.country.title().strip())
 
-    city_db[str(new_object.city_id)] = new_object.dict()
+    city_db[new_object.city_id] = new_object.dict()
 
     return new_object
 
@@ -247,13 +245,13 @@ async def add_city(city_details: CitySave):
             responses={"404": {"code": "404", "message": "Not Found"},
                        "409": {"code": "409", "message": "Conflict"}})
 async def delete_city(city_id: UUID):
-    find_city = city_db.get(str(city_id))
+    find_city = city_db.get(city_id)
     if find_city:
         check = is_assigned(search_id=find_city["city_id"])
         if check:
             return JSONResponse(status_code=409, content={"code": "409",
                                                           "message": "City is assigned to queen"})
-        del city_db[str(city_id)]
+        del city_db[city_id]
         return {}
 
     return JSONResponse(status_code=404, content={"code": "404",
@@ -267,11 +265,12 @@ async def delete_city(city_id: UUID):
                     "404": {"code": "404", "message": "Not Found"}},
          response_model=City)
 async def update_city(city_id: UUID, update_data: CitySave):
-    find_city = city_db.get(str(city_id))
+    find_city = city_db.get(city_id)
     if find_city:
         clean_name = update_data.name.title().strip()
 
-        check = is_unique(search=clean_name, in_key="name", db=city_db)
+        check = is_unique(search=clean_name, in_key="name", db=city_db,
+                          given_id=city_id, pk_name="city_id")
         if not check:
             return JSONResponse(status_code=400, content={"code": "400",
                                                           "message": "Name must be unique"})
@@ -284,7 +283,7 @@ async def update_city(city_id: UUID, update_data: CitySave):
 
         find_city.update(new_data_cleaned)
 
-        update_city_in_queens_db(str(city_id), new_data_cleaned)
+        update_city_in_queens_db(city_id, new_data_cleaned)
         return find_city
 
     return JSONResponse(status_code=404, content={"code": "404",
@@ -309,7 +308,7 @@ async def get_all_categories():
          responses={"404": {"code": "404", "message": "Not Found"}},
          response_model=Category)
 async def get_category(category_id: UUID):
-    result = category_db.get(str(category_id))
+    result = category_db.get(category_id)
     if result:
         return result
     return JSONResponse(status_code=404, content={"code": "404", "message": "Not Found"})
@@ -329,14 +328,13 @@ async def add_category(category_details: CategorySave):
         return JSONResponse(status_code=400,
                             content={"code": "400", "message": "Name must be unique"})
 
-    if not is_able_to_add_to_db(category_db):
+    if not able_to_add_to_db(category_db):
         return JSONResponse(status_code=400,
                             content={"code": "400", "message": "Number of items in db was exceeded"})
 
-    new_object = Category(category_id=generate_new_uuid4(),
-                          name=category_details.name.lower().strip())
+    new_object = Category(name=category_details.name.lower().strip())
 
-    category_db[str(new_object.category_id)] = new_object.dict()
+    category_db[new_object.category_id] = new_object.dict()
 
     return new_object
 
@@ -346,10 +344,10 @@ async def add_category(category_details: CategorySave):
             status_code=204,
             responses={"404": {"code": "404", "message": "Not Found"}})
 async def delete_category(category_id: UUID):
-    find_category = category_db.get(str(category_id))
+    find_category = category_db.get(category_id)
     if find_category:
-        del category_db[str(category_id)]
-        delete_tag_from_queens_db(str(category_id))
+        del category_db[category_id]
+        delete_tag_from_queens_db(category_id)
         return {}
 
     return JSONResponse(status_code=404, content={"code": "404",
@@ -363,7 +361,7 @@ async def delete_category(category_id: UUID):
                     "404": {"code": "404", "message": "Not Found"}},
          response_model=Category)
 async def update_category(category_id: UUID, update_data: CategorySave):
-    find_category = category_db.get(str(category_id))
+    find_category = category_db.get(category_id)
     if find_category:
         check = is_unique(search=update_data.name.lower().strip(), in_key="name", db=category_db)
         if not check:
@@ -373,7 +371,7 @@ async def update_category(category_id: UUID, update_data: CategorySave):
         clean_name = update_data.name.lower().strip()
         find_category["name"] = clean_name
 
-        update_tag_name_in_queens_db(str(category_id), clean_name)
+        update_tag_name_in_queens_db(category_id, clean_name)
         return find_category
 
     return JSONResponse(status_code=404, content={"code": "404",
