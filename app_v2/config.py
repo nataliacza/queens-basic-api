a498887910ed7aea7a83570d3207fdc3d318fa1b
Natalia Czapska <czapskanatalia@gmail.com>
2022-11-14 18:56:59 +0100

    Add db limit and expiry date to db items

--- app_v2/config.py
+++ app_v2/config.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from dotenv import load_dotenv
 from pydantic import BaseSettings
@@ -7,9 +8,9 @@ load_dotenv()
 
 class Settings(BaseSettings):
     title: str = "Drag Queens Collection API - v2"
-    description: str = """This is a Drag Queens Collection API documentation based on the OpenApi
-                       specification. Main goal of the project is to create simple database
-                       available for everybody."""
+    description: str = """Drag Queens Collection API is an application, using which you can search for information
+    about Drag Queens in Poland. Main goal of the project is to create lightweight and simple database
+    available for everybody."""
     version: str = "2.0"
 
     deta_project_key: str = os.environ.get("DETA_PROJECT_KEY")
@@ -17,6 +18,9 @@ class Settings(BaseSettings):
     jwt_secret: str = os.environ.get("JWT_SECRET")
     jwt_expiry_minutes: int = int(os.environ.get("JWT_EXPIRY_MINUTES", 5))
     jwt_expiry_hours: int = int(os.environ.get("JWT_EXPIRY_HOURS", 1))
+
+    db_limit: int = 20
+    db_item_expire_at = datetime.datetime.fromisoformat("2023-02-01T00:00:00")
 
     class Config:
         env_file = ".env"
