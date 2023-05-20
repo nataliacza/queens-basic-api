fb4509640a7f84dbe709284c4e1d1022aca53de3
Natalia Czapska <czapskanatalia@gmail.com>
2023-05-20 17:00:04 +0200

    feat: update config file

--- app_v2/config.py
+++ app_v2/config.py
@@ -1,31 +1,32 @@
-import os
 import datetime
+import os
 
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
-    title: str = "Drag Queens Collection API - v2"
+    title: str = "Drag Queens Collection API"
     description: str = """Drag Queens Collection API is an application, using which 
     you can search for information about Drag Queens in Poland. Main goal 
     of the project is to create lightweight and simple db available for everybody."""
-    version: str = "2.0"
+    version: str = os.environ.get("PROJECT_VERSION", "0.0.0")
 
-    deta_project_key: str = os.environ.get("DETA_PROJECT_KEY")
+    deta_project_key: str = os.environ.get("DETA_PROJECT_KEY", "no-key")
 
-    token_algorithm: str = os.environ.get("TOKEN_ALGORITHM")
-    token_type: str = os.environ.get("TOKEN_TYPE")
-    token_issuer: str = os.environ.get("TOKEN_ISSUER")
-    jwt_secret: str = os.environ.get("JWT_SECRET")
+    token_algorithm: str = os.environ.get("TOKEN_ALGORITHM", "no-algorithm")
+    token_type: str = os.environ.get("TOKEN_TYPE", "no-type")
+    token_issuer: str = os.environ.get("TOKEN_ISSUER", "no-issuer")
+    jwt_secret: str = os.environ.get("JWT_SECRET", "no-secret")
     jwt_expiry_minutes: int = int(os.environ.get("JWT_EXPIRY_MINUTES", 5))
     jwt_expiry_hours: int = int(os.environ.get("JWT_EXPIRY_HOURS", 1))
 
-    db_limit: int = 20
-    db_item_expire_at = datetime.datetime.fromisoformat("2023-02-01T00:00:00")
+    db_limit: int = int(os.environ.get("DATABASE_LIMIT", 5))
+    db_item_expire_at = datetime.datetime.fromisoformat("2023-10-01T00:00:00")
 
-    cors_origins = os.environ.get("ORIGINS")
+    cors_origins: list = list(os.environ.get("ORIGINS", []))
 
     class Config:
         env_file = ".env"
